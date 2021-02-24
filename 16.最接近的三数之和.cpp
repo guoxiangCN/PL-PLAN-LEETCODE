f780c8c95aa1daee1effc145fc335788df941d19
guoxiang2 <guoxiang2@yy.com>
2021-02-24 16:26:15 +0800

    msg

--- 16.最接近的三数之和.cpp
+++ 16.最接近的三数之和.cpp
@@ -13,9 +13,27 @@ class Solution {
 public:
   int threeSumClosest(vector<int> &nums, int target) {
     std::sort(nums.begin(), nums.end());
-    int sum = 0;
+    int ret = nums[0]+nums[1]+nums[2];
+    int sum=0;
+    for(int i = 0; i <= nums.size(); i++) {
+      int L = i+1;
+      int R = nums.size()-1;
+      while(L < R) {
+        sum = nums[i] + nums[L] + nums[R];
+        if(sum == target) {
+          return sum;
+        } else if(sum < target) {
+          L++;
+        } else {
+          R--;
+        }
+        if(std::abs(sum - target) < std::abs(ret - target)) {
+          ret = sum;
+        }
+      }
+    }
 
-    return sum;
+    return ret;
   }
 };
 // @lc code=end
