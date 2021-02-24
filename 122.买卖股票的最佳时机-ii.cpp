9ed1b8d2455081aefc0bdfffa3861000e224ae85
guoxiang2 <guoxiang2@yy.com>
2021-02-24 18:00:58 +0800

    a

--- 122.买卖股票的最佳时机-ii.cpp
+++ 122.买卖股票的最佳时机-ii.cpp
@@ -3,12 +3,22 @@
  *
  * [122] 买卖股票的最佳时机 II
  */
+ #include <vector>
+ using std::vector;
 
 // @lc code=start
 class Solution {
 public:
     int maxProfit(vector<int>& prices) {
-
+        int maxProfit = 0;
+        
+        for(int i = 1; i < prices.size(); i++) {
+            if(prices[i]- prices[i-1] > 0) {
+                maxProfit += (prices[i]- prices[i-1]);
+            }
+        }
+        
+        return maxProfit;
     }
 };
 // @lc code=end
